1371f6b6853142de17c2e9ab2640cb26a03ff7e9
erikredmo <erikredmo@gmail.com>
2024-10-17 16:15:49 +0200

    add file writing

--- assignment3/newton_1.c
+++ assignment3/newton_1.c
@@ -12,8 +12,8 @@ typedef struct {
 } int_padded;
 
 typedef struct {
-//  uint8_t ** attractors;
-//  uint8_t ** convergences;
+  uint8_t ** attractors;
+  uint8_t ** convergences;
   int ib;
   int istep;
   int sz;
@@ -24,8 +24,8 @@ typedef struct {
 } thrd_info_t;
 
 typedef struct {
-//  uint8_t ** attractors;
-//  uint8_t ** convergences;
+  uint8_t ** attractors;
+  uint8_t ** convergences;
   int sz;
   int nthrds;
   mtx_t *mtx;
@@ -35,19 +35,9 @@ typedef struct {
 
 // GLOBAL DECLERATION
 int nthrds, sz, d;
-uint8_t ** attractors;
-uint8_t ** convergences;
-char * colors[] = {"128 0 32\n", "34 85 50\n", "40 65 135\n", "204 153 0\n", "54 117 136\n", "75 0 130\n", "204 85 0\n"};
-/*const char *colors[] = {
-    "#800020",  // Deep Burgundy
-    "#225532",  // Forest Green
-    "#284187",  // Royal Blue
-    "#CC9900",  // Mustard Yellow
-    "#367588",  // Teal
-    "#4B0082",  // Aubergine
-    "#CC5500"   // Burnt Sienna
-};
-*/
+//uint8_t ** attractors;
+//uint8_t ** convergences;
+
 
 int
 main_thrd(
@@ -55,8 +45,8 @@ main_thrd(
     )
 {
   const thrd_info_t *thrd_info = (thrd_info_t*) args;
-//  uint8_t ** attractors = thrd_info->attractors;
-//  uint8_t ** convergences = thrd_info->convergences;
+  uint8_t ** attractors = thrd_info->attractors;
+  uint8_t ** convergences = thrd_info->convergences;
   const int ib = thrd_info->ib;
   const int istep = thrd_info->istep;
   const int sz = thrd_info->sz;
@@ -71,8 +61,8 @@ main_thrd(
     uint8_t * attractor = (uint8_t*) malloc(sz*sizeof(uint8_t));
 
     for ( int jx = 0; jx < sz; ++jx ) {
-      attractor[jx] = 0;
-      convergence[jx] = 0;
+      attractor[jx] = ix;
+      convergence[jx] = ix;
     }
     mtx_lock(mtx);
     attractors[ix] = attractor;
@@ -92,31 +82,14 @@ main_thrd_check(
     )
 {
   const thrd_info_check_t *thrd_info = (thrd_info_check_t*) args;
-//  uint8_t ** attractors = thrd_info->attractors;
-//  uint8_t **convergences = thrd_info->convergences;
+  uint8_t ** attractors = thrd_info->attractors;
+  uint8_t **convergences = thrd_info->convergences;
   const int sz = thrd_info->sz;
   const int nthrds = thrd_info->nthrds;
   mtx_t *mtx = thrd_info->mtx;
   cnd_t *cnd = thrd_info->cnd;
   int_padded *status = thrd_info->status;
 
-  char att_file[24];
-  char conv_file[25];
-  sprintf(att_file, "newton_attractors_x%d.ppm", d);
-  sprintf(conv_file, "newton_convergence_x%d.ppm", d);
-  FILE *fp_att = fopen(att_file, "w");
-  FILE *fp_conv = fopen(conv_file, "w");
-  if (fp_att == NULL) {
-    printf("Error opening file\n");
-    return -1; 
-  }
-  if (fp_conv == NULL) {
-    printf("Error opening file\n");
-    return -1; 
-  }
-  fprintf(fp_att, "P3\n%d %d\n255\n", sz, sz);
-  fprintf(fp_conv, "P3\n%d %d\n50\n", sz, sz);
- 
   for ( int ix = 0, ibnd; ix < sz; ) {
     for ( mtx_lock(mtx); ; ) {
       ibnd = sz;
@@ -136,21 +109,14 @@ main_thrd_check(
 
     // Det är här vi vill skriva till vår fil sen
     for ( ; ix < ibnd; ++ix ) {
-      for ( int jx = 0; jx < sz; ++jx ) {
-        // printf(colors[attractors[ix][jx]]);
-        fwrite(colors[attractors[ix][jx]], sizeof(char), strlen(colors[attractors[ix][jx]]), fp_att);
-        char grey_color[50];
-        sprintf(grey_color, "%d %d %d\n", convergences[ix][jx], convergences[ix][jx], convergences[ix][jx]);
-        fwrite(grey_color, sizeof(char), strlen(grey_color), fp_conv);
-
-      }
+      for ( int jx = 0; jx < sz; ++jx )
+        printf("%u ", attractors[ix][jx]);
       free(attractors[ix]);
       free(convergences[ix]);
       printf("\n");
     }
   }
-  fclose(fp_att);
-  fclose(fp_conv);
+
   return 0;
 }
 
@@ -190,8 +156,8 @@ main(
   }
 
 
-  attractors = (uint8_t**) malloc(sz*sizeof(uint8_t*));
-  convergences = (uint8_t**) malloc(sz*sizeof(uint8_t*));
+  uint8_t ** attractors = (uint8_t**) malloc(sz*sizeof(uint8_t*));
+  uint8_t ** convergences = (uint8_t**) malloc(sz*sizeof(uint8_t*));
   /*
   float *ventries = (float*) malloc(sz*sz*sizeof(float));
 
@@ -215,11 +181,10 @@ main(
 
   int_padded status[nthrds];
 
-   
   // Här gör vi threads
   for ( int tx = 0; tx < nthrds; ++tx ) {
-//    thrds_info[tx].attractors = attractors;
-//    thrds_info[tx].convergences = convergences;
+    thrds_info[tx].attractors = attractors;
+    thrds_info[tx].convergences = convergences;
     thrds_info[tx].ib = tx;
     thrds_info[tx].istep = nthrds;
     thrds_info[tx].sz = sz;
@@ -238,8 +203,8 @@ main(
   }
 
   {
-//    thrd_info_check.attractors = attractors;
-//    thrd_info_check.convergences = convergences;
+    thrd_info_check.attractors = attractors;
+    thrd_info_check.convergences = convergences;
     thrd_info_check.sz = sz;
     thrd_info_check.nthrds = nthrds;
     thrd_info_check.mtx = &mtx;
@@ -265,7 +230,6 @@ main(
 
   mtx_destroy(&mtx);
   cnd_destroy(&cnd);
-
 
   return 0;
 }
