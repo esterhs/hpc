a0e98aea0782db24a95c303a7eb16f53d9047e14
erikredmo <erikredmo@gmail.com>
2024-10-18 14:32:00 +0200

    add comp and image

--- assignment3/newton.c
+++ assignment3/newton.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <threads.h>
 #include <stdint.h>
+#include <complex.h>
 
 typedef struct {
   int val;
@@ -16,7 +17,7 @@ typedef struct {
 //  uint8_t ** convergences;
   int ib;
   int istep;
-  int sz;
+  int size;
   int tx;
   mtx_t *mtx;
   cnd_t *cnd;
@@ -26,7 +27,7 @@ typedef struct {
 typedef struct {
 //  uint8_t ** attractors;
 //  uint8_t ** convergences;
-  int sz;
+  int size;
   int nthrds;
   mtx_t *mtx;
   cnd_t *cnd;
@@ -34,20 +35,12 @@ typedef struct {
 } thrd_info_check_t;
 
 // GLOBAL DECLERATION
-int nthrds, sz, d;
+int nthrds, size, d;
 uint8_t ** attractors;
 uint8_t ** convergences;
-char * colors[] = {"128 0 32\n", "34 85 50\n", "40 65 135\n", "204 153 0\n", "54 117 136\n", "75 0 130\n", "204 85 0\n"};
-/*const char *colors[] = {
-    "#800020",  // Deep Burgundy
-    "#225532",  // Forest Green
-    "#284187",  // Royal Blue
-    "#CC9900",  // Mustard Yellow
-    "#367588",  // Teal
-    "#4B0082",  // Aubergine
-    "#CC5500"   // Burnt Sienna
-};
-*/
+char * colors[] = {"128 0 32\n", "34 85 50\n", "40 65 135\n", "204 153 0\n", "54 117 136\n", "75 0 130\n", "204 85 0\n", "255 255 255\n"};
+double * roots_re;
+double * roots_im;
 
 int
 main_thrd(
@@ -59,20 +52,81 @@ main_thrd(
 //  uint8_t ** convergences = thrd_info->convergences;
   const int ib = thrd_info->ib;
   const int istep = thrd_info->istep;
-  const int sz = thrd_info->sz;
+  const int size = thrd_info->size;
   const int tx = thrd_info->tx;
   mtx_t *mtx = thrd_info->mtx;
   cnd_t *cnd = thrd_info->cnd;
   int_padded *status = thrd_info->status;
+  uint8_t conv, attr;
 
+  for ( int ix = ib; ix < size; ix += istep ) {
+    uint8_t * convergence = (uint8_t*) malloc(size*sizeof(uint8_t));
+    uint8_t * attractor = (uint8_t*) malloc(size*sizeof(uint8_t));
 
-  for ( int ix = ib; ix < sz; ix += istep ) {
-    uint8_t * convergence = (uint8_t*) malloc(sz*sizeof(uint8_t));
-    uint8_t * attractor = (uint8_t*) malloc(sz*sizeof(uint8_t));
+    double im = -2 + (double) ix * 4 / (double) size;
 
-    for ( int jx = 0; jx < sz; ++jx ) {
-      attractor[jx] = 0;
-      convergence[jx] = 0;
+    for ( int jx = 0; jx < size; ++jx ) {
+
+      double re = -2 + (double) jx * 4 / (double) size;
+      double complex x = re + im * I;
+      for ( conv = 0, attr = 8; ; ++conv ) {
+        if ( conv > 50 ) {
+          attr = 7;
+          break;
+        } 
+        if ( creal(x) > 10e5 || creal(x) < -10e5 || cimag(x) > 10e5 || cimag(x) < -10e5 ) { //upper bound
+          attr = 7;
+          break;
+        }
+        if ( creal(x) * creal(x) + cimag(x) *cimag(x) < 10e-6) { //lower bound
+          attr = 7;
+          break;
+        }
+        for ( int kx = 0; kx < d; ++kx ) {
+          double diff_re = ( creal(x) - roots_re[kx]) * ( creal(x) - roots_re[kx]);
+          double diff_im = ( cimag(x) - roots_im[kx]) * ( cimag(x) - roots_im[kx]);
+          double dist = diff_re + diff_im;
+          if ( dist < 10e-6 ) {
+            attr = kx;
+            break;
+          }
+        }
+        if ( attr != 8 )
+          break;
+        
+        double complex x_old = x;
+        //printf("x = %f + i%f\n", creal(x), cimag(x));
+        switch ( d ) {
+          case 1:
+           x = 1;
+           break;
+          case 2:
+           x = x_old - 0.5 * x_old + 0.5 / x_old;
+           break;
+          case 3:
+           x = x_old - (1./3.) * x_old + (1./3.) / (x_old * x_old);
+           break;
+          case 4:
+           x = x_old - (1./4.) * x_old + (1./4.) / (x_old * x_old * x_old);
+           break;
+          case 5:
+           x = x_old - (1./5.) * x_old + (1./5.) / (x_old * x_old * x_old * x_old);
+           break;
+          case 6:
+           x = x_old - (1./6.) * x_old + (1./6.) / (x_old * x_old * x_old * x_old * x_old);
+           break;
+          case 7:
+           x = x_old - (1./7.) * x_old + (1./7.) / (x_old * x_old * x_old * x_old * x_old * x_old);
+           break;
+          default:
+           fprintf(stderr, "unexpcted degree\n");
+           exit(1);
+        }
+      }
+
+    attractor[jx] = attr;
+    convergence[jx] = conv;
+
     }
     mtx_lock(mtx);
     attractors[ix] = attractor;
@@ -94,7 +148,7 @@ main_thrd_check(
   const thrd_info_check_t *thrd_info = (thrd_info_check_t*) args;
 //  uint8_t ** attractors = thrd_info->attractors;
 //  uint8_t **convergences = thrd_info->convergences;
-  const int sz = thrd_info->sz;
+  const int size = thrd_info->size;
   const int nthrds = thrd_info->nthrds;
   mtx_t *mtx = thrd_info->mtx;
   cnd_t *cnd = thrd_info->cnd;
@@ -114,12 +168,12 @@ main_thrd_check(
     printf("Error opening file\n");
     return -1; 
   }
-  fprintf(fp_att, "P3\n%d %d\n255\n", sz, sz);
-  fprintf(fp_conv, "P3\n%d %d\n50\n", sz, sz);
+  fprintf(fp_att, "P3\n%d %d\n255\n", size, size);
+  fprintf(fp_conv, "P3\n%d %d\n50\n", size, size);
  
-  for ( int ix = 0, ibnd; ix < sz; ) {
+  for ( int ix = 0, ibnd; ix < size; ) {
     for ( mtx_lock(mtx); ; ) {
-      ibnd = sz;
+      ibnd = size;
       for ( int tx = 0; tx < nthrds; ++tx )
         if ( ibnd > status[tx].val )
           ibnd = status[tx].val;
@@ -136,8 +190,8 @@ main_thrd_check(
 
     // Det är här vi vill skriva till vår fil sen
     for ( ; ix < ibnd; ++ix ) {
-      for ( int jx = 0; jx < sz; ++jx ) {
-        // printf(colors[attractors[ix][jx]]);
+      for ( int jx = 0; jx < size; ++jx ) {
+        //printf("%u\n", attractors[ix][jx]);
         fwrite(colors[attractors[ix][jx]], sizeof(char), strlen(colors[attractors[ix][jx]]), fp_att);
         char grey_color[50];
         sprintf(grey_color, "%d %d %d\n", convergences[ix][jx], convergences[ix][jx], convergences[ix][jx]);
@@ -162,12 +216,12 @@ main(
     )
 {
   // default values
-  nthrds = 1;
-  sz = 1;
+  nthrds = 5;
+  size = 10;
   d = 1;
   int opt;
   
-  //const int sz = 500;
+  //const int size = 500;
 
   while ((opt = getopt(argc,argv, "t:l:")) != -1) {
     switch (opt) {
@@ -175,7 +229,7 @@ main(
         nthrds = atoi(optarg);
         break;
       case 'l':
-        sz = atoi(optarg);
+        size = atoi(optarg);
         break;
       default:
         printf("Usage: -t -l d");
@@ -189,16 +243,115 @@ main(
     printf("Missing d.");
   }
 
+  roots_re = (double*)malloc(d * sizeof(double));
+  roots_im = (double*)malloc(d * sizeof(double));
+  
+  switch ( d ) {
+    case 1:
+      roots_re[0] = 1;
+      roots_im[0] = 0;
+      break;
+    case 2: 
+      roots_re[0] = 1;
+      roots_im[0] = 0;
+      
+      roots_re[1] = -1;
+      roots_im[1] = 0;
+      break;
+    case 3:
+      roots_re[0] = 1;
+      roots_im[0] = 0;
+      
+      roots_re[1] = -0.5;
+      roots_im[1] = -0.86603;
 
-  attractors = (uint8_t**) malloc(sz*sizeof(uint8_t*));
-  convergences = (uint8_t**) malloc(sz*sizeof(uint8_t*));
+      roots_re[2] = -0.5;
+      roots_im[2] = 0.86603;      
+      break;
+    case 4:
+      roots_re[0] = 1;
+      roots_im[0] = 0;
+      
+      roots_re[1] = -1;
+      roots_im[1] = 0;
+
+      roots_re[2] = 0;
+      roots_im[2] = 1; 
+
+      roots_re[3] = 0;
+      roots_im[3] = 1;
+      break;
+    case 5:
+      roots_re[0] = 1;
+      roots_im[0] = 0;
+      
+      roots_re[1] = -0.80902;
+      roots_im[1] = -0.58779;
+
+      roots_re[2] = -0.80902;
+      roots_im[2] = 0.58779;
+
+      roots_re[3] = 0.30902;
+      roots_im[3] = -0.95106; 
+
+      roots_re[4] = 0.30902;
+      roots_im[4] = 0.95106;
+      break;
+    case 6:
+      roots_re[0] = 1;
+      roots_im[0] = 0;
+
+      roots_re[1] = -1;
+      roots_im[1] = 0;
+
+      roots_re[2] = 0.5;
+      roots_im[2] = 0.86603;
+
+      roots_re[3] = -0.5;
+      roots_im[3] = -0.86603;
+
+      roots_re[4] = -0.5;
+      roots_im[4] = 0.86603;
+
+      roots_re[5] = 0.5;
+      roots_im[5] = -0.86603;
+      break;
+    case 7:
+      roots_re[0] = 1;
+      roots_im[0] = 0;
+
+      roots_re[1] = -0.90097;
+      roots_im[1] = -0.43388;
+      
+      roots_re[2] = -0.90097;
+      roots_im[2] = 0.43388;
+
+      roots_re[3] = 0.62349;
+      roots_im[3] = 0.78183;
+
+      roots_re[4] = 0.62349;
+      roots_im[4] = -0.78183;
+
+      roots_re[5] = -0.22252;
+      roots_im[5] = -0.97493;
+
+      roots_re[6] = -0.22252;
+      roots_im[6] = 0.97493;
+      break;
+    default:
+      fprintf(stderr, "unexpcted degree\n");
+      exit(1);
+  }
+
+  attractors = (uint8_t**) malloc(size*sizeof(uint8_t*));
+  convergences = (uint8_t**) malloc(size*sizeof(uint8_t*));
   /*
-  float *ventries = (float*) malloc(sz*sz*sizeof(float));
+  float *ventries = (float*) malloc(size*size*sizeof(float));
 
-  for ( int ix = 0, jx = 0; ix < sz; ++ix, jx += sz )
+  for ( int ix = 0, jx = 0; ix < size; ++ix, jx += size )
     v[ix] = ventries + jx;
 
-  for ( int ix = 0; ix < sz*sz; ++ix )
+  for ( int ix = 0; ix < size*size; ++ix )
     ventries[ix] = ix;
 */
   thrd_t thrds[nthrds];
@@ -222,7 +375,7 @@ main(
 //    thrds_info[tx].convergences = convergences;
     thrds_info[tx].ib = tx;
     thrds_info[tx].istep = nthrds;
-    thrds_info[tx].sz = sz;
+    thrds_info[tx].size = size;
     thrds_info[tx].tx = tx;
     thrds_info[tx].mtx = &mtx;
     thrds_info[tx].cnd = &cnd;
@@ -240,7 +393,7 @@ main(
   {
 //    thrd_info_check.attractors = attractors;
 //    thrd_info_check.convergences = convergences;
-    thrd_info_check.sz = sz;
+    thrd_info_check.size = size;
     thrd_info_check.nthrds = nthrds;
     thrd_info_check.mtx = &mtx;
     thrd_info_check.cnd = &cnd;
@@ -262,6 +415,9 @@ main(
   //free(ventries);
   free(convergences);
   free(attractors);
+
+  free(roots_re);
+  free(roots_im);
 
   mtx_destroy(&mtx);
   cnd_destroy(&cnd);
